21f46c8e43b39e6764cd7c9006fe79c257e13d89
Shivani Ghosle <shivani@webkorps.com>
2022-08-05 15:56:47 +0530

    url changes

--- src/main/java/com/codecompiler/controller/StudentController.java
+++ src/main/java/com/codecompiler/controller/StudentController.java
@@ -1,6 +1,5 @@
 package com.codecompiler.controller;
 
-import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.collections4.map.HashedMap;
@@ -12,7 +11,6 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
