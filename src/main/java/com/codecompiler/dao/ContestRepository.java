e5c3d6bcfb3da0e9a8fe135781e808b8ec1f75a6
Shivani Ghosle <shivani@webkorps.com>
2022-08-17 20:53:04 +0530

    cors polocy

--- src/main/java/com/codecompiler/dao/ContestRepository.java
+++ src/main/java/com/codecompiler/dao/ContestRepository.java
@@ -1,5 +1,7 @@
 package com.codecompiler.dao;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +14,7 @@ public interface ContestRepository extends  MongoRepository<Contest,String>{
 	public Contest findByContestIdAndContestLevel(String contestid, String contestLevel);
 	
 	public Contest findByContestId(String id);
+	
+	public List<Contest> findAll();
 	
 }
--- src/main/java/com/codecompiler/service/ContestService.java
+++ src/main/java/com/codecompiler/service/ContestService.java
@@ -1,8 +1,15 @@
 package com.codecompiler.service;
 
+import java.util.List;
+
 import com.codecompiler.entity.Contest;
 
 public interface ContestService {
 
 	public Contest findByContestId(String contestId);
+	
+	public List<Contest> findAllContest();
+	
+	public Contest saveContest(Contest contest); 
+	
 }
